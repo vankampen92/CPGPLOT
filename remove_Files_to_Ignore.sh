ea24ac860095dbd65b7fd46825ac4c2b87d35eb0
vankampen92 <dalonso@ceab.csic.es>
2020-06-06 00:02:10 +0200

    Correcion on makefile of CPGPLOT_EXAMPLES

--- remove_Files_to_Ignore.sh
+++ remove_Files_to_Ignore.sh
@@ -6,6 +6,6 @@ find . -type f -name '*.ps' -delete
 find . -type f -name '*.eps' -delete
 # find . -type f -name '*.pdf' -delete
 find . -type f -name '*.so' -delete
-# find . -type f -name '*.a' -delete
+find . -type f -name '*.a' -delete
 find . -type f -name '*~' -delete
 find . -type f -executable -delete
